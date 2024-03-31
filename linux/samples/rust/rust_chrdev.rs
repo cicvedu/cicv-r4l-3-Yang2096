a6e5848cbfe091a3c9089faefd6e44ec50a263b7
Yang2096 <yang2096@outlook.com>
2024-04-01 00:43:13 +0800

    feat: assignment 5

--- linux/samples/rust/rust_chrdev.rs
+++ linux/samples/rust/rust_chrdev.rs
@@ -2,6 +2,7 @@
 
 //! Rust character device sample.
 
+use core::ops::Deref;
 use core::result::Result::Err;
 
 use kernel::prelude::*;
@@ -22,9 +23,19 @@ static GLOBALMEM_BUF: Mutex<[u8;GLOBALMEM_SIZE]> = unsafe {
     Mutex::new([0u8;GLOBALMEM_SIZE])
 };
 
+static BUF_TAIL: Mutex<usize> = unsafe {
+    Mutex::new(0)
+};
+
+static BUF_HEAD: Mutex<usize> = unsafe {
+    Mutex::new(0)
+};
+
 struct RustFile {
     #[allow(dead_code)]
     inner: &'static Mutex<[u8;GLOBALMEM_SIZE]>,
+    tail: &'static Mutex<usize>,
+    head: &'static Mutex<usize>,
 }
 
 #[vtable]
@@ -34,17 +45,45 @@ impl file::Operations for RustFile {
     fn open(_shared: &(), _file: &file::File) -> Result<Box<Self>> {
         Ok(
             Box::try_new(RustFile {
-                inner: &GLOBALMEM_BUF
+                inner: &GLOBALMEM_BUF,
+                tail: &BUF_TAIL,
+                head: &BUF_HEAD,
             })?
         )
     }
 
     fn write(_this: &Self,_file: &file::File,_reader: &mut impl kernel::io_buffer::IoBufferReader,_offset:u64,) -> Result<usize> {
-        Err(EPERM)
+        let data = _reader.read_all()?;
+        pr_info!("got write request, data input {}", data.len());
+        let mut inner = _this.inner.lock();
+        let mut tail = _this.tail.lock();
+
+        let mut len = data.len();
+        if *tail + len >= GLOBALMEM_SIZE {
+            len = GLOBALMEM_SIZE - *tail;
+        }
+        for i in 0..len {
+            inner[i+*tail] = data[i];
+        }
+        *tail += len;
+        Ok(data.len())
     }
 
     fn read(_this: &Self,_file: &file::File,_writer: &mut impl kernel::io_buffer::IoBufferWriter,_offset:u64,) -> Result<usize> {
-        Err(EPERM)
+        let inner = _this.inner.lock();
+        let mut tail = _this.tail.lock();
+        let mut head = _this.head.lock();
+        
+        let len = *tail - *head;
+        pr_info!("got read request, data remaining {}", len);
+        if len > 0 {
+            _writer.write_slice(&inner.deref()[*head..*tail])?;
+            *head = *tail;
+        } else {
+            *tail = 0;
+            *head = 0;
+        }
+        Ok(len)
     }
 }
 
